37023f7e1ce7738920afe1c1c7c59f844902e899
GrahamQuan <grahamquan24@gmail.com>
2024-04-02 23:19:58 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
   return (
     <>
       <h1>ENV: {process.env.NEXT_TITLE}</h1>
-      <p>test code</p>
+      <p>test vercel ci/cd</p>
       <section className="grid grid-cols-1 md:grid-cols-3 gap-1 justify-center items-center p-16 sm:justify-items-center">
         {PhotosRSC}
       </section>
